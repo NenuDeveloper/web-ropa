7c45cccc14848bd2cdf33804f6c30ecc36a99bff
Ángel Ordóñez Sánchez <nenust1@outlook.com>
2019-01-23 20:19:21 -0600

    Updates

---
+++ precache-manifest.699f440cfc466eea8d039aa4a272a5b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12596045372c6d5453b9",
+    "url": "/web-ropa/static/css/main.388e4c8e.chunk.css"
+  },
+  {
+    "revision": "12596045372c6d5453b9",
+    "url": "/web-ropa/static/js/main.12596045.chunk.js"
+  },
+  {
+    "revision": "b3e73632672d19d1bd38",
+    "url": "/web-ropa/static/js/1.b3e73632.chunk.js"
+  },
+  {
+    "revision": "9bf1f4f6ee8f72ad0faf",
+    "url": "/web-ropa/static/js/runtime~main.9bf1f4f6.js"
+  },
+  {
+    "revision": "6f9482a62d5356d95cf684ae4806344b",
+    "url": "/web-ropa/index.html"
+  }
+];
